3f964938a1d6a9e566c87eede0ef313a7caaf15f
Gabtongues <aladebusayo.g@gmail.com>
2025-07-29 20:54:27 +0100

    arithemtic_operations.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,5 +5,6 @@ elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
-else:
+else:../python_introduction/simple_interest.py
+
     print("Sorry, I don't have recommendations for this weather.")
